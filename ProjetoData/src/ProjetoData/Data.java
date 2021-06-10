43dc16c64fad6ad00c2b4a97111d1896b93ffb35
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-10 07:50:16 -0300

    Acrescentando Calendar

--- ProjetoData/src/ProjetoData/Data.java
+++ ProjetoData/src/ProjetoData/Data.java
@@ -2,6 +2,7 @@ package ProjetoData;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Data {
@@ -34,11 +35,12 @@ public class Data {
 			break;
 		}
 
+		System.out.println("Data em Milissegundos: " + date.getTime());
 		System.out.println("Dia da Semana: " + dia);
 		System.out.println("Data " + date.getDate() + "/" + date.getMonth() + "/" + (date.getYear() + 1900));
-		System.out.println("Horário " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+		System.out.println("Horário " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "\n");
 
-		/*************** SimpleDateFormat ***************/
+		/********************* SimpleDateFormat *********************/
 
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy"); // This format to show the comum user
 		System.out.println("Data " + simpleDateFormat.format(date));
@@ -46,7 +48,17 @@ public class Data {
 		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss"); // This format to save in DB
 		System.out.println("Data DB: " + simpleDateFormat.format(date));
 
-		System.out.println("Formato Data: " + simpleDateFormat.parse("1125-02-17 14:30.0"));
+		System.out.println("Objeto Data: " + simpleDateFormat.parse("1125-02-17 14:30.0") + "\n");
 
+		/********************* Calendar *********************/
+
+		Calendar calendar = Calendar.getInstance();
+
+		System.out.println("Calendar em Mili: " + calendar.getTimeInMillis());
+		System.out.println("Calendar Dia da Semana: " + calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println("Calendar Data: " + calendar.get(Calendar.DAY_OF_MONTH) + "/" + calendar.get(Calendar.MONTH)
+				+ "/" + calendar.get(Calendar.YEAR));
+		System.out.println("Calendar Horário: " + calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE)
+				+ ":" + calendar.get(Calendar.SECOND));
 	}
 }
