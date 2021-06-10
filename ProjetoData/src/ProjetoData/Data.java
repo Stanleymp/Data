471c4afcd15299aeb86795cc8783b9d41bb73404
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-10 07:52:34 -0300

    Organizando

--- ProjetoData/src/ProjetoData/Data.java
+++ ProjetoData/src/ProjetoData/Data.java
@@ -8,35 +8,12 @@ import java.util.Date;
 public class Data {
 	public static void main(String[] args) throws ParseException {
 
-		Date date = new Date();
-		String dia = "";
+		/*************************** Date ***************************/
 
-		switch (date.getDay()) {
-		case 0:
-			dia = "Domingo";
-			break;
-		case 1:
-			dia = "Segunda-Feira";
-			break;
-		case 2:
-			dia = "Terça-Feira";
-			break;
-		case 3:
-			dia = "Quarta-Feira";
-			break;
-		case 4:
-			dia = "Quinta-Feira";
-			break;
-		case 5:
-			dia = "Sexta-Feira";
-			break;
-		case 6:
-			dia = "Sábado";
-			break;
-		}
+		Date date = new Date();
 
 		System.out.println("Data em Milissegundos: " + date.getTime());
-		System.out.println("Dia da Semana: " + dia);
+		System.out.println("Dia da Semana: " + date.getTime());
 		System.out.println("Data " + date.getDate() + "/" + date.getMonth() + "/" + (date.getYear() + 1900));
 		System.out.println("Horário " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "\n");
 
@@ -50,7 +27,7 @@ public class Data {
 
 		System.out.println("Objeto Data: " + simpleDateFormat.parse("1125-02-17 14:30.0") + "\n");
 
-		/********************* Calendar *********************/
+		/************************* Calendar *************************/
 
 		Calendar calendar = Calendar.getInstance();
 
