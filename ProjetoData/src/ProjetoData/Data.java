d9766f0b769d41f21a89470991ac4bd65fb05dce
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-10 06:35:08 -0300

    Organização do Switch

--- ProjetoData/src/ProjetoData/Data.java
+++ ProjetoData/src/ProjetoData/Data.java
@@ -9,27 +9,13 @@ public class Data {
 		String dia = "";
 
 		switch (date.getDay()) {
-		case 0:
-			dia = "Domingo";
-			break;
-		case 1:
-			dia = "Segunda-Feira";
-			break;
-		case 2:
-			dia = "Terça-Feira";
-			break;
-		case 3:
-			dia = "Quarta-Feira";
-			break;
-		case 4:
-			dia = "Quinta-Feira";
-			break;
-		case 5:
-			dia = "Sexta-Feira";
-			break;
-		case 6:
-			dia = "Sábado";
-			break;
+		case 0: dia = "Domingo"; break;
+		case 1: dia = "Segunda-Feira"; break;
+		case 2: dia = "Terça-Feira"; break;
+		case 3: dia = "Quarta-Feira"; break;
+		case 4: dia = "Quinta-Feira"; break;
+		case 5: dia = "Sexta-Feira"; break;
+		case 6: dia = "Sábado"; break;
 		}
 
 		System.out.println("Dia da Semana: " + dia);
