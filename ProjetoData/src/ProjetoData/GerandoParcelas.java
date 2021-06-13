0a6977aaac8f651632c9e673564fd2f488665b59
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-13 13:19:23 -0300

    Adicionando Boleto

--- ProjetoData/src/ProjetoData/GerandoParcelas.java
+++ ProjetoData/src/ProjetoData/GerandoParcelas.java
@@ -9,8 +9,17 @@ public class GerandoParcelas {
 
 	public static void main(String[] args) throws ParseException {
 
-		Date vencimentoBoleto = new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021");
+		Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("15/07/2021");
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dataInicial);
+
+		for (int parcela = 1; parcela <= 12; parcela++) {
+			calendar.add(Calendar.MONTH, 1);
+
+			System.out.println("Data da " + parcela + "ª parcela é: "
+					+ new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}
 
 	}
-
 }
