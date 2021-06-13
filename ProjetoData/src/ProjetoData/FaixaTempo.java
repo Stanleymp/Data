d99df04ea129d72922c226917c48a8afc4d3d6ba
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-13 14:19:21 -0300

    Faixa de Tempo com Mensagem

--- ProjetoData/src/ProjetoData/FaixaTempo.java
+++ ProjetoData/src/ProjetoData/FaixaTempo.java
@@ -5,12 +5,23 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
+import java.util.Date;
 
 public class FaixaTempo {
 	public static void main(String[] args) throws ParseException {
 
-		Long dias = ChronoUnit.DAYS.between(LocalDate.parse("1452-06-15"), LocalDate.now());
-		System.out.println("Possuem " + dias + " dias entre a faixa de datas.");
-		
+		Long diasAusente = ChronoUnit.DAYS.between(LocalDate.parse("2021-06-12"), LocalDate.now());
+
+		System.out.println(diasAusente + (diasAusente <= 1 ? " Dia Ausente" : " Dias Ausentes"));
+
+		if (diasAusente >= 10) {
+			System.out.println("Bonus Perdido!");
+		} else if (diasAusente > 7) {
+			System.out.println("Por favor! Logue na plataforma para atualizar seu Bonus!");
+			System.out.println("Perderá o Bonus em " + (10 - diasAusente) + (diasAusente >= 9 ? " dia" : " dias"));
+		} else {
+			System.out.println("Bonus em dia!");
+		}
+
 	}
 }
