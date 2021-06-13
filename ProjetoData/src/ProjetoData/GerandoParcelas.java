714eb333f0830b0127384ae93b83960c05d57c88
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-13 13:18:26 -0300

    Simulando Boleto

---
+++ ProjetoData/src/ProjetoData/GerandoParcelas.java
@@ -0,0 +1,16 @@
+package ProjetoData;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class GerandoParcelas {
+
+	public static void main(String[] args) throws ParseException {
+
+		Date vencimentoBoleto = new SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021");
+
+	}
+
+}
