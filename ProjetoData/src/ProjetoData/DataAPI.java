0289f7230b1468f2d32840a65ddadf5857cb1f30
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-13 15:17:17 -0300

    Puxando Dia, Mês e Ano da API

--- ProjetoData/src/ProjetoData/DataAPI.java
+++ ProjetoData/src/ProjetoData/DataAPI.java
@@ -6,8 +6,12 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.swing.text.DateFormatter;
+
 public class DataAPI {
 	public static void main(String[] args) {
+
+		/* WORKING WITH LOCALDATE API */
 
 		LocalDate dataAtual = LocalDate.now();
 		LocalTime horaAtual = LocalTime.now();
@@ -15,7 +19,16 @@ public class DataAPI {
 
 		System.out.println("Data Atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		System.out.println("Hora Atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-		System.out.println("Data e Hora Atual: " + dtHrAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+		System.out
+				.println("Data e Hora Atual: " + dtHrAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+
+		/* WORKING WITH MONTHS, DAYS AND YEARS */
+
+		System.out.println("Dia da Semana: " + dtHrAtual.getDayOfWeek());
+		System.out.println("Dia Atual: " + dtHrAtual.getDayOfMonth());
+		System.out.println("Mês Atual: " + dtHrAtual.getMonth());
+		System.out.println("Ano Atual: " + dtHrAtual.getYear());
+		System.out.println("Dia do Ano: " + dtHrAtual.getDayOfYear());
 
 	}
 }
