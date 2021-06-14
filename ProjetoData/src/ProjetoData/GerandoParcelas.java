90b3cf386f5bace6f84f59f9674211c86147c7f4
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-13 22:39:07 -0300

    Acrscentando outra forma de geração com API LocalDate

--- ProjetoData/src/ProjetoData/GerandoParcelas.java
+++ ProjetoData/src/ProjetoData/GerandoParcelas.java
@@ -2,6 +2,8 @@ package ProjetoData;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -9,16 +11,26 @@ public class GerandoParcelas {
 
 	public static void main(String[] args) throws ParseException {
 
-		Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("15/07/2021");
+		/* USING CALENDAR */
+		/*
+		 * Calendar calendar = Calendar.getInstance(); calendar.setTime(new
+		 * SimpleDateFormat("dd/MM/yyyy").parse("15/06/2021"));
+		 * 
+		 * for (int parcela = 1; parcela <= 12; parcela++) {
+		 * calendar.add(Calendar.MONTH, 1); System.out.println(parcela + "ª Parcela: " +
+		 * new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		 * 
+		 * }
+		 */
 
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(dataInicial);
+		/* USING LOCALDATE AND PLUS */
+
+		LocalDate dataAtual = LocalDate.now();
 
 		for (int parcela = 1; parcela <= 12; parcela++) {
-			calendar.add(Calendar.MONTH, 1);
-
-			System.out.println("Data da " + parcela + "ª parcela é: "
-					+ new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+			dataAtual = dataAtual.plusMonths(1);
+			System.out.println(
+					parcela + "ª com vencimento em: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 		}
 
 	}
