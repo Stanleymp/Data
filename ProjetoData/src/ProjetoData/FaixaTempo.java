88bc99a5ca595ab491f69d7dc1330fa2e949c446
stanl <stanl@DESKTOP-RLK6MCS>
2021-06-13 21:53:58 -0300

    Acrescentando Período

--- ProjetoData/src/ProjetoData/FaixaTempo.java
+++ ProjetoData/src/ProjetoData/FaixaTempo.java
@@ -5,16 +5,15 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
-import sun.jvm.hotspot.runtime.Thread;
-
 public class FaixaTempo {
 	public static void main(String[] args) throws ParseException, InterruptedException {
 
-		/* WORKING WITH DATES */
+		/* WORKING WITH DATES - CHRONOUNIT */
 
 		Long diasAusente = ChronoUnit.DAYS.between(LocalDate.parse("2021-06-12"), LocalDate.now());
 		int bonus = 15;
@@ -28,17 +27,31 @@ public class FaixaTempo {
 			System.out.println(
 					"Perderá o Bonus em " + (bonus - diasAusente) + (diasAusente >= (bonus - 1) ? " dia" : " dias"));
 		} else {
-			System.out.println("Bonus em dia!");
+			System.out.println("Bonus em dia!" + "\n");
 		}
 
-		/* WORKING WITH TIMES */
+		/* WORKING WITH TIMES INSTANT AND DURATION */
 
 		Instant inicio = Instant.now();
 		Thread.sleep(2000);
 		Instant ifinal = Instant.now();
 		Duration duracao = Duration.between(inicio, ifinal);
 
-		System.out.println("Duração em Mili Segundos: " + duracao.toMillis());
+		System.out.println("Duração em Mili Segundos: " + duracao.toMillis() + "\n");
+
+		/* WORKING WITH LOCALDATE TO COMPARE DATES */
+
+		LocalDate dataAntiga = LocalDate.of(2020, 02, 17);
+		LocalDate dataNova = LocalDate.of(2021, 06, 12);
+
+		System.out.println("A Data Antiga é Depois que a Data Nova? " + dataAntiga.isAfter(dataNova));
+		System.out.println("A Data Antiga é Antes que a Data Nova? " + dataAntiga.isBefore(dataNova));
+		System.out.println("A Data Antiga é Igual a Data Nova? " + dataAntiga.isEqual(dataNova) + "\n");
+
+		/* USING PERIOD */
+
+		Period periodo = Period.between(dataAntiga, dataNova);
+		System.out.println("O período entre a Data Antiga e a Data Nova é: " + periodo);
 
 	}
 }
